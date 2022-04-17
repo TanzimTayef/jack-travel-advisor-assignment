0876d64b8ccb2678e30abbcf6080817bee745cf8
TanzimTayef <tayef464@gmail.com>
2022-04-17 23:42:16 +0600

    added banner design

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,19 +1,34 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import "./Banner.css";
+import banner from "../../../images/banner.png";
 
 const Banner = () => {
   return (
-    <div id="banner" className="d-flex align-items-center">
-      <Container>
-        <h1 className="display-4 fw-bold">Explore The World!</h1>
-        <p className="mt-2">
-          You can create a Custom Trip.Search Our Lowest Fares to Your Favorite <br />
-          Destinations.Find a better way to travel
-              </p>
-              <button class="hero-btn">Explore More</button>
-      </Container>
-    </div>
+    <div id="banner">
+         <Container>
+      <div 
+        style={{ height: "80vh" }}
+        className="d-md-flex align-items-center justify-content-between"
+      >
+        <div className="banner-left my-5 my-md-0">
+          <h1 className="display-4 fw-bold">Explore The World!</h1>
+          <p className="mt-2">
+            You can create a Custom Trip.Search Our Lowest Fares to Your
+            Favorite <br />
+            Destinations.Find a better way to travel
+          </p>
+          <button class="hero-btn">Explore More</button>
+        </div>
+        <div className="banner-right d-flex my-5 my-md-0 justify-content-end">
+          <div>
+            <img src={banner} className="img-fluid" alt="" />
+          </div>
+        </div>
+      </div>
+    </Container>
+   </div>
+    
   );
 };
 
--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
   };
 
   return (
-    <Navbar bg="white" expand="lg" style={{ height: "60px" }}>
+    <Navbar bg="white" expand="lg" style={{ height: "80px" }}>
       <Container>
         <Navbar.Brand to="/home" className="fw-bold">
           Jack Travel Advisor
