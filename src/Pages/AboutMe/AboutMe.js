cd1c5879d524a8677a1ac52abf1fff03f868f451
TanzimTayef <tayef464@gmail.com>
2022-04-17 20:34:46 +0600

    added about me page some description about me

--- src/Pages/AboutMe/AboutMe.js
+++ src/Pages/AboutMe/AboutMe.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 const AboutMe = () => {
-    return (
-        <div>
-            This is about me
-        </div>
-    );
+  return (
+    <div>
+      <Container>
+        <Row>
+                  <Col>
+                      <div className="my-desc">
+                          <h1>Tanzim Tayef</h1>
+                          <p>I want to be a full stack web developer. This is my goal. i want to achive it in this year. i have enough cofidence. i'm already join with this course for filup my gaol. i'm already fecing vary tarp situcation. 
+                          after achived skill web developmet then i want to go join with a company. i want to a job. 
+                          </p>
+                      </div>
+                  </Col>
+                  <Col>
+                      <div>
+                          <img src="" alt="img" />
+                      </div>
+                  </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default AboutMe;
