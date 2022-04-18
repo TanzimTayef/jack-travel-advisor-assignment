bd7e9149cbc1267db183e1c572631d3954063d6f
TanzimTayef <tayef464@gmail.com>
2022-04-18 15:21:16 +0600

    check login and signin fuctionality

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -29,32 +29,36 @@ const Signup = () => {
       setError("Your email don't match");
       return;
     }
-
-    // creating user by firebase hooks
-    createUserWithEmailAndPassword(email, password);
-    console.log("send email");
-
     if (user) {
       navigate("/");
     }
+
+    // creating user by firebase hooks
+    createUserWithEmailAndPassword(email, password);
+    setName("")
+    setEmail("")
+    setPassword("")
+    setConfirmpassword("")
   };
 
   return (
     <div className="text-center py-5">
       <h3 className="form-title">CREATE YOUR ACCOUNT</h3>
       <div className="form-container">
-        <form>
-          <input type="text" name="name" placeholder="User name" id="" />
+        <form onSubmit={handleSignupForm}>
+          <input type="text"  onBlur={(e) => setName(e.target.value)} name="name" placeholder="User name" id="" />
           <input
             type="email"
+            onBlur={(e) => setEmail(e.target.value)}
             name="email"
             placeholder="Enter your email address"
             id=""
           />
-          <input type="password" name="password" placeholder="Password" id="" />
+          <input type="password"  onBlur={(e) => setPassword(e.target.value)}  name="password" placeholder="Password" id="" />
+          <input type="password"  onBlur={(e) => setConfirmpassword(e.target.value)}  name="password" placeholder="Confirm Password" id="" />
           <div>
             <button className="sing-btn" type="submit">
-              Log In
+              Sign In
             </button>
           </div>
         </form>
@@ -62,56 +66,6 @@ const Signup = () => {
       <p className="my-3">Already have an account?? <Link className="fs-5" to="/login">Login</Link></p>
       <OtherLogin />
     </div>
-
-    // <div>
-    //   <h1 className="text-center mt-3">Sign Up</h1>
-    //   <div className="w-50 mx-auto mt-3">
-    //     <Form onSubmit={handleSignupForm}>
-    //       <Form.Group className="mb-3" controlId="formBasicEmail">
-    //         <Form.Control
-    //           onBlur={(e) => setName(e.target.value)}
-    //           type="text"
-    //           placeholder="user name"
-    //         />
-    //       </Form.Group>
-    //       <Form.Group className="mb-3" controlId="formBasicEmail">
-    //         <Form.Control
-    //           onBlur={(e) => setEmail(e.target.value)}
-    //           type="email"
-    //           placeholder="enter email"
-    //         />
-    //         <Form.Text className="text-muted">
-    //           We'll never share your email with anyone else.
-    //         </Form.Text>
-    //       </Form.Group>
-    //       <Form.Group className="mb-3" controlId="formBasicPassword">
-    //         <Form.Control
-    //           onBlur={(e) => setPassword(e.target.value)}
-    //           type="password"
-    //           placeholder="password"
-    //         />
-    //       </Form.Group>
-    //       <Form.Group className="mb-3" controlId="formBasicPassword">
-    //         <Form.Control
-    //           onBlur={(e) => setConfirmpassword(e.target.value)}
-    //           type="password"
-    //           placeholder="password"
-    //         />
-    //       </Form.Group>
-    //       <Form.Group className="mb-3" controlId="formBasicCheckbox">
-    //         <Form.Check type="checkbox" label="Check me out" />
-    //       </Form.Group>
-    //       {error}
-    //       <Button variant="primary" type="submit">
-    //         Submit
-    //       </Button>
-    //     </Form>
-    //     <p>
-    //       Already have an account? <Link to="/login">Login</Link>{" "}
-    //     </p>
-    //   </div>
-    //   <OtherLogin />
-    // </div>
   );
 };
 
